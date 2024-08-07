2a9b798bb43e684353343b386bcb3f01ed61cc4a
Jonathan Preston <jonathan.c.preston@gmail.com>
2024-08-07 14:26:47 +0100

    Initial Commit

---
+++ MeritCard/MeritCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeritCardApp.swift
+//  MeritCard
+//
+//  Created by Jonathan Preston on 07/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MeritCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
